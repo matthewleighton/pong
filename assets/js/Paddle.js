7dd88aaea9035ad6bf12d2be5d79181dadc592b5
Matthew <matthewrleighton@gmail.com>
2017-09-17 23:39:59 +0100

    Begin adding movement logic

--- assets/js/Paddle.js
+++ assets/js/Paddle.js
@@ -3,13 +3,23 @@ class Paddle extends Physical {
 	constructor(physicalSettings, config) {		
 		super(physicalSettings, config);
 
+		this.objectType = 'paddle';
+
 		this.xFromOrigin = this.percentageToPixel(0.7);
 		this.yFromOrigin = this.percentageToPixel(10);
 		
 		this.originX = (this.playerNumber == 1 ? this.percentageToPixel(5) : this.percentageToPixel(95));
 		this.originY = this.percentageToPixel(50);
 
-		this.drawSquares();
+		this.drawSquares('paddle');
+
+		for (var i = 0; i < 10; i++) {
+			
+			window.setTimeout(() => {
+				this.moveObject(0, 0.5);
+			},3000);
+		}
+
 	}
 
 }
--- assets/js/Physical.js
+++ assets/js/Physical.js
@@ -32,31 +32,48 @@ class Physical {
 	}
 
 	percentageToPixel(percentage) {
-		return Math.floor((this.resolution / 100) * percentage);
+		if (percentage == 0) return 0;
+
+		var pixel = Math.floor((this.resolution / 100) * percentage);
+		if (pixel == 0) return 1;
+
+		console.log(percentage);
+		console.log((this.resolution / 100) * percentage);
+
+		return pixel;
 	}
 
 	// This draws an object by coloring every square between the object's top left and bottom right.
-	drawSquares() {
+	drawSquares(squareType) {
 		var topLeft = {
 			x: this.originX - this.xFromOrigin,
 			y: this.originY - this.yFromOrigin
 		};
-		this.grid.getSquare(topLeft['x'], topLeft['y']).setSquareType('test');
 
 		var bottomRight = {
 			x: this.originX + this.xFromOrigin,
 			y: this.originY + this.yFromOrigin
 		};
-		this.grid.getSquare(bottomRight['x'], bottomRight['y']).setSquareType('test');
 
 		for (var x = topLeft['x']; x <= bottomRight['x']; x++) {
 			for (var y = topLeft['y']; y <= bottomRight['y']; y++) {
-				this.grid.getSquare(x, y).setSquareType('paddle');
+				this.grid.getSquare(x, y).setSquareType(squareType);
 			}
 		}
 
 		// Test line - remove later.
 		this.grid.getSquare(this.originX, this.originY).setSquareType('test');
+	}
+
+	moveObject(xChange, yChange) {
+		this.drawSquares('empty');
+		this.grid.getSquare(this.originX, this.originY).setSquareType('test');
+
+		console.log(this.percentageToPixel(xChange));
+
+		this.originX += this.percentageToPixel(xChange);
+		this.originY += this.percentageToPixel(yChange);
+		this.drawSquares(this.objectType);
 	}
 
 }
