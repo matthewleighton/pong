16415f279dff0d60b3f2c26a223db7e7f3b12e9d
Matthew <matthewrleighton@gmail.com>
2017-09-17 20:58:01 +0100

    Auto resize game on window resize

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,6 +1,9 @@
 class Game {
 	constructor(config) {
 
+		// This is needed because 'this' in window resize event listener refers to the window, not the Game object.
+		self = this;
+
 		// Dynamically assign object variables based on items in config object.
 		for (var key in config) {
 			if (config.hasOwnProperty(key)) {
@@ -8,7 +11,36 @@ class Game {
 			};
 		}
 
+
+		this.containerElement = document.getElementById(this.container);
+
 		console.log(this);
 
+
+		this.setGameSize();
+		
+		window.addEventListener('resize', this.setGameSize);
 	}
+
+	setGameSize() {
+		var w = window,
+			d = document,
+			e = d.documentElement,
+			g = d.getElementsByTagName('body')[0],
+			width = w.innerWidth || e.clientWidth || g.clientWidth,
+			height = w.innerHeight || e.clientHeight || g.clientHeight;
+
+		console.log(self);
+
+		var container = document.getElementById(self.container);
+
+		container.style.height = height + 'px';
+		container.style.width = width + 'px';
+
+		console.log(container);
+
+		console.log('width: ' + width);
+		console.log('height: ' + height);
+	}
+
 }
